ad44b3dad5fc5d0bff323612bf237b4dd5054171
Mae Brick Manuel <maebrickm@gmail.com>
2017-01-27 21:50:08 +0800

    commited project3io

---
+++ project3io.java
@@ -0,0 +1,11 @@
+class project3io{
+	public static void main( String [] args )
+
+		for( String i : inputs ){
+			System.out.println(i);
+
+		System.out.print("There are " +args.length);
+		System.out.println(" arguments.");
+		}
+	}
+}
